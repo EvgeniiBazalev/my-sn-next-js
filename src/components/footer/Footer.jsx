a103fc7ddd2c6cc06720e19c54d38cd081683ae3
EvgeniiBazalev <bazalevj@gmail.com>
2023-09-28 10:25:52 +0500

    step 4.1 footer + homepage animation

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,12 +1,18 @@
 import styles from './Footer.module.css'
+import Image from 'next/image';
 
 const Footer = () => {
   return (
-    <footer>
-        <div className={styles.container}>2023 My social network</div>
-        <div></div>
+    <footer className={styles.container}>
+      <div>2023 MyApp. All rights reserved</div>
+      <div className={styles.social}>
+        <Image src='/vk.png' width={15} height={15} className={styles.icon} alt="vk.com"/>
+        <Image src="/inst.png" width={15} height={15} className={styles.icon} alt="instagram"/>
+        <Image src="/twitter.png" width={15} height={15} className={styles.icon} alt="twitter"/>
+        <Image src="/yt.png" width={15} height={15} className={styles.icon} alt="youtube"/>
+      </div>
     </footer>
-  )
+  );
 }
 
 export default Footer
