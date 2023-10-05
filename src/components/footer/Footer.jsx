b0b7a0f5c1f3ef464cac4e6df9a71e5310c14ade
EvgeniiBazalev <bazalevj@gmail.com>
2023-10-05 15:36:43 +0500

    step 5.1 portfolio + [id]

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,18 +1,42 @@
-import styles from './Footer.module.css'
-import Image from 'next/image';
+import styles from "./Footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
   return (
     <footer className={styles.container}>
       <div>2023 MyApp. All rights reserved</div>
       <div className={styles.social}>
-        <Image src='/vk.png' width={15} height={15} className={styles.icon} alt="vk.com"/>
-        <Image src="/inst.png" width={15} height={15} className={styles.icon} alt="instagram"/>
-        <Image src="/twitter.png" width={15} height={15} className={styles.icon} alt="twitter"/>
-        <Image src="/yt.png" width={15} height={15} className={styles.icon} alt="youtube"/>
+        <Image
+          src="/vk.png"
+          width={30}
+          height={30}
+          className={styles.icon}
+          alt="vk.com"
+        />
+        <Image
+          src="/inst.png"
+          width={30}
+          height={30}
+          className={styles.icon}
+          alt="instagram"
+        />
+        <Image
+          src="/twitter.png"
+          width={30}
+          height={30}
+          className={styles.icon}
+          alt="twitter"
+        />
+        <Image
+          src="/yt.png"
+          width={30}
+          height={30}
+          className={styles.icon}
+          alt="youtube"
+        />
       </div>
     </footer>
   );
-}
+};
 
-export default Footer
+export default Footer;
