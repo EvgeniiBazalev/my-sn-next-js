5ec38724e6d7862fbf31bd9b04b7b92967e372ab
EvgeniiBazalev <bazalevj@gmail.com>
2023-10-06 16:33:56 +0500

    step 5 darkmode

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,53 +1,66 @@
-'use client'
+"use client";
 
-import Link from "next/link"
-import styles from './Navbar.module.css'
+import Link from "next/link";
+import DarkModeToggle from "../DarkModeToggle/DarkModeToggle";
+import styles from "./Navbar.module.css";
 
 const Navbar = () => {
-    const links = [
-        {
-          id: 1,
-          title: "Home",
-          url: "/",
-        },
-        {
-          id: 2,
-          title: "Portfolio",
-          url: "/portfolio",
-        },
-        {
-          id: 3,
-          title: "Blog",
-          url: "/blog",
-        },
-        {
-          id: 4,
-          title: "About",
-          url: "/about",
-        },
-        {
-          id: 5,
-          title: "Contact",
-          url: "/contact",
-        },
-        {
-          id: 6,
-          title: "Dashboard",
-          url: "/dashboard",
-        },
-      ];
-    
+  const links = [
+    {
+      id: 1,
+      title: "Home",
+      url: "/",
+    },
+    {
+      id: 2,
+      title: "Portfolio",
+      url: "/portfolio",
+    },
+    {
+      id: 3,
+      title: "Blog",
+      url: "/blog",
+    },
+    {
+      id: 4,
+      title: "About",
+      url: "/about",
+    },
+    {
+      id: 5,
+      title: "Contact",
+      url: "/contact",
+    },
+    {
+      id: 6,
+      title: "Dashboard",
+      url: "/dashboard",
+    },
+  ];
+
   return (
     <nav className={styles.container}>
-        <Link href='' className={styles.logo}>MyApp</Link>
-        <div className={styles.link}>
-            {links.map((link) => (
-                <Link key={link.id} href={link.url}>{link.title}</Link>
-            ))}
-            <button className={styles.logout} onClick={() => {console.log('logget out')}}>Logout</button>
-        </div>
+      <Link href="" className={styles.logo}>
+        MyApp
+      </Link>
+      <div className={styles.link}>
+        <DarkModeToggle />
+        {links.map((link) => (
+          <Link key={link.id} href={link.url}>
+            {link.title}
+          </Link>
+        ))}
+        <button
+          className={styles.logout}
+          onClick={() => {
+            console.log("logget out");
+          }}
+        >
+          Logout
+        </button>
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
